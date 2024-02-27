77dd2cefd7c2ca5ca599fad39db52365184160f8
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-26 19:30:08 -0500

    token especifico

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -60,9 +60,8 @@ export const AuthProvider = ({ children }) => {
     try {
       const res = await loginRequest(user);
       // Establecer la cookie específica
-      Cookies.set("miCookie", "00001212", { expires: 1 });
-      // Guardar el token en las cookies
-      Cookies.set("token", res.data.token, { expires: 1 }); // Ejemplo de configuración de la cookie con una duración de 1 día
+      Cookies.set("token", "00001212", { expires: 1 });
+       // Ejemplo de configuración de la cookie con una duración de 1 día
       setIsAuthenticated(true);
       setUser(res.data.user); // Guardar los datos del usuario si es necesario
     } catch (error) {
