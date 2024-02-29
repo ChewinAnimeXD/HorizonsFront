815ea73eb03611dec5c6a12ff1de4eeff56bc46b
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-29 18:48:11 -0500

    hg

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,16 +1,25 @@
-
 import axios from "./axios";
 
 export const registerRequest = (user) => axios.post(`/register`, user);
 
-export const loginRequest = (user) => axios.post(`/login`, user);
 
-export const verityTokenRequest = () => axios.get('/verify');
+export const loginRequest = (user) => {
+  return axios.post(`/login`, user).then((response) => {
+    const token = response.data.token;
+    console.log("aca esta el token", token)
+    axios.defaults.headers.common["autentification"] = token;
+
+    return response;
+  });
+};
+
+export const verityTokenRequest = () => axios.get("/verify");
 
 export const deleteUserRequest = async (id) => axios.delete(`/userPage/${id}`);
 
 export const getUsersRequest = async () => axios.get("/userPage");
 
-export const updateUserRequest = async (id, user) => axios.put(`/register/${id}`, user);
+export const updateUserRequest = async (id, user) =>
+  axios.put(`/register/${id}`, user);
 
 export const getUserRequest = async (id) => axios.get(`/register/${id}`);
