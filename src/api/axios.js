e3cc811e7246680f8c716e46375c3593970c0cd2
Edwin Camilo Sanabria Torres <edwin.sanabria03@uptc.edu.co>
2024-02-29 09:52:54 -0500

    cambios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://backend-horizons.vercel.app/api',
+    baseURL: 'http://localhost:4000/api',
     withCredentials: true
 })
 
